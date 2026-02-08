06492b28868b059c69137c90d662484ee4f830e6
MNIGARR <nigarrmustafazada@gmail.com>
2026-02-08 00:52:59 +0000

    Changed the some of spaces

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,21 +16,21 @@ void bubble_sort(int *array, size_t size)
 
     if (array == NULL || size < 2)
         return;
-    
+
     for (i = 0; i < size - 1; i++)
     {
         swapped = 0;
-
+        
         for (j = 0; j < size - i - 1; j++)
         {
-            if (array[j] > array[j + 1]) 
+            if (array[j] > array[j + 1])
             {
                 temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
 
                 print_array(array, size);
-
+                
                 swapped = 1;
             }
         }
